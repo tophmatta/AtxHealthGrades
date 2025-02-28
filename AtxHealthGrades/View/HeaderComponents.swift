3398a5b75a2621767c1f3e83a3c1ac6d095b9e6d
Toph Matta <toph.matta@gmail.com>
2025-02-27 19:55:12 -0500

    Add animations and haptics

--- AtxHealthGrades/View/HeaderComponents.swift
+++ AtxHealthGrades/View/HeaderComponents.swift
@@ -10,6 +10,7 @@ struct HeaderComponents: View {
     @Binding var segmentedSelection: Segment
     @State private var text: String = ""
     @FocusState private var isSearchFocused: Bool
+    @State private var isButtonPressed = false
     
     let radiusSearchAction: () -> ()
     let textSearchAction: (String) -> ()
@@ -41,7 +42,13 @@ struct HeaderComponents: View {
     @ViewBuilder
     private var radiusSearchButton: some View {
         Button {
+            isButtonPressed = true
             radiusSearchAction()
+            Task { @MainActor in
+                // Non-blocking because it suspends the current task rather than blocking the thread.
+                try? await Task.sleep(for: .seconds(0.2))
+                isButtonPressed = false
+            }
         } label: {
             Text("Search Area")
                 .padding(5)
@@ -53,6 +60,10 @@ struct HeaderComponents: View {
         .buttonBorderShape(.capsule)
         .buttonStyle(.borderedProminent)
         .shadow(radius: 5)
+        .sensoryFeedback(.success, trigger: isButtonPressed)
+        .scaleEffect(isButtonPressed ? 0.9 : 1.0) // Slight shrink effect on press
+        .animation(.spring(response: 0.3, dampingFraction: 0.7), value: isButtonPressed) // Smooth animation
+
         .transition(.asymmetric(insertion: .push(from: .trailing), removal: .opacity))
     }
     
@@ -69,12 +80,15 @@ struct HeaderComponents: View {
             .foregroundStyle(.onSurface)
             .focused($isSearchFocused)
             .onSubmit {
+                triggerHapticFeedback()
                 textSearchAction(text)
                 isSearchFocused = false
             }
             .keyboardType(.webSearch)
             .disableAutocorrection(true)
             .ignoresSafeArea(.keyboard)
+            .scaleEffect(isSearchFocused ? 1.05 : 1.0)
+            .animation(.spring(response: 0.3, dampingFraction: 0.7), value: isSearchFocused)
             .transition(.asymmetric(insertion: .push(from: .leading), removal: .opacity))
     }
     
@@ -86,6 +100,11 @@ struct HeaderComponents: View {
             .onTapGesture {
                 isSearchFocused = false
             }
+    }
+    
+    private func triggerHapticFeedback() {
+        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+        impactMed.impactOccurred()
     }
 }
 
